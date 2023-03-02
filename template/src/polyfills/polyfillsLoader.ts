823852ed8457cedd28e993f1c4ed6a6684f8f16a
amaiorov <maiorov.artem@gmail.com>
2023-03-02 17:10:45 +0100

    fix polyfill loader

--- template/src/polyfills/polyfillsLoader.ts
+++ template/src/polyfills/polyfillsLoader.ts
@@ -1,6 +1,8 @@
-export async function LoadPolyfillsIfNeeded() {
+async function LoadPolyfillsIfNeeded() {
   if (!(globalThis && "URLPattern" in globalThis)) {
     const path = "/scripts/polyfills/urlpatternPolyfill.js";
     await import(path);
   }
 }
+
+export const polyfillsLoaded = LoadPolyfillsIfNeeded();
